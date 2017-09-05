0340fd5f37692bb7743ae942281837b4a7a60ce5
Marcel Fahle <m.fahle@gmail.com>
2017-09-05 12:13:25 +0300

    ak3 landing complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+var ls = document.getElementsByClassName( 'section--linked' );
+for (var i = 0; i<ls.length; i++) {
+  var anchors = ls[i].getElementsByClassName('setlist__item-order-button');
+  for(var j=0; j < anchors.length; j++){
+    anchors[j].onclick = function(e){ e.preventDefault();}
+  }
+  ls[i].onclick = function() {
+    //location.href = this.dataset.href;
+    window.open(this.dataset.href, '_self' );
+  }
+}
+
